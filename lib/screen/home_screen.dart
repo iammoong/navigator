52207163f4e79e0a570b5c88d6bc4ce4351d1ad2
iammoong <ansrlwkd225@naver.com>
2024-02-15 00:25:39 +0900

    Initial commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeScreen'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Push',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
