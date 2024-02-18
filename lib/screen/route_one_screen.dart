400a429d3557cf95956c1d1fbd0c975642fdcc89
iammoong <ansrlwkd225@naver.com>
2024-02-18 22:52:32 +0900

    navigator 추가 및 객체화

--- lib/screen/route_one_screen.dart
+++ lib/screen/route_one_screen.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:navigator/layout/main_layout.dart';
+import 'package:navigator/screen/route_two_screen.dart';
 
 class RouteOneScreen extends StatelessWidget {
-  const RouteOneScreen({Key? key}) : super(key: key);
+  final int number;
+
+  const RouteOneScreen({
+    required this.number,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MainLayout(title: 'Route One', children: [
+      Text(
+        'arguments : ${number.toString()}',
+        textAlign: TextAlign.center,
+      ),
       ElevatedButton(
           onPressed: () {
-            Navigator.of(context).pop();
+            // [HomeScreen(), RouteOne(), RouteTwo()]
+            // 생성이 될 때는 오른쪽 부터, 지워질 때는 왼쪽 으로 데이터가 지워진다.
+            // 위와 같은 데이터 구조를 스택(Stack) 이라고 한다.
+            Navigator.of(context).pop(456);
           },
           child: Text('Pop')),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => RouteTwoScreen(),
+              settings: RouteSettings(
+                arguments: 789,
+              ),
+            ),
+          );
+        },
+        child: Text('Push'),
+      ),
     ]);
   }
 }
--- lib/screen/route_two_screen.dart
+++ lib/screen/route_two_screen.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:navigator/layout/main_layout.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Text('Route Two'),
-    )
+    final arguments = ModalRoute.of(context)!.settings.arguments;
+    return MainLayout(
+        title: 'Route Two',
+        children: [
+          Text('arguments : ${arguments}',
+            textAlign: TextAlign.center,
+          ),
+          ElevatedButton(onPressed: (){
+            Navigator.of(context).pop();
+          }, 
+              child: Text('Pop')
+          ),
+        ],
     );
   }
 }
